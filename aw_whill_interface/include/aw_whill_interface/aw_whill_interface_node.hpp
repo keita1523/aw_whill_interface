460ed6c33e94c7e2a6e1a603db9b744d2fe30467
keita1523 <keita.miura@tier4.jp>
2023-01-11 03:37:40 +0900

    feat: add aw_whill_interface

--- aw_whill_interface/include/aw_whill_interface/aw_whill_interface_node.hpp
+++ aw_whill_interface/include/aw_whill_interface/aw_whill_interface_node.hpp
@@ -1,12 +1,26 @@
-# include <rclcpp/rclcpp.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <autoware_auto_control_msgs/msg/ackermann_control_command.hpp>
+#include "sensor_msgs/msg/joy.hpp"
 
 namespace sample_module
 {
+  using AwCmdType = autoware_auto_control_msgs::msg::AckermannControlCommand;
+  using WhillCmdType = sensor_msgs::msg::Joy;
 
 class AwWhillInterface : public rclcpp::Node
 {
 public:
   explicit AwWhillInterface(const rclcpp::NodeOptions & node_options);
+
+private:
+  void callbackAwCmd(const AwCmdType::ConstSharedPtr & msg);
+
+
+  rclcpp::Subscription<AwCmdType>::SharedPtr sub_autoware_cmd_;
+
+
+  rclcpp::Publisher<WhillCmdType>::SharedPtr pub_whill_cmd_;
 
 };
 
--- aw_whill_interface/src/aw_whill_interface_node.cpp
+++ aw_whill_interface/src/aw_whill_interface_node.cpp
@@ -7,8 +7,26 @@ AwWhillInterface::AwWhillInterface(const rclcpp::NodeOptions & node_options)
 : Node("aw_whill_interface", node_options)
 {
   RCLCPP_INFO(this->get_logger(), "AW WHILL interface launched.");
+
+  sub_autoware_cmd_ = this->create_subscription<AwCmdType>(
+    "~/input/autoware_command",
+    rclcpp::QoS(1),
+    std::bind(&AwWhillInterface::callbackAwCmd, this, std::placeholders::_1)
+  );
+
+  pub_whill_cmd_ = this->create_publisher<WhillCmdType>("~/output/whill_command", rclcpp::QoS(1));
+
 }
 
+void AwWhillInterface::callbackAwCmd(const AwCmdType::ConstSharedPtr & msg)
+{
+  WhillCmdType output_cmd;
+  output_cmd.axes.push_back(msg->lateral.steering_tire_angle);
+  output_cmd.axes.push_back(msg->longitudinal.speed);
+  pub_whill_cmd_->publish(output_cmd);
+
+  return;
+}
 
 } // namespace sample_module
 
