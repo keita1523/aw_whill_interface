6f176576bad8ee03a47879971eea227928633f07
keita1523 <keita.miura@tier4.jp>
2023-01-24 23:42:44 +0900

    pre feedback

--- aw_whill_interface/include/aw_whill_interface/aw_whill_interface_node.hpp
+++ aw_whill_interface/include/aw_whill_interface/aw_whill_interface_node.hpp
@@ -1,14 +1,22 @@
 #include <rclcpp/rclcpp.hpp>
 
 #include <autoware_auto_control_msgs/msg/ackermann_control_command.hpp>
+#include <autoware_auto_vehicle_msgs/msg/steering_report.hpp>
+#include <autoware_auto_vehicle_msgs/msg/velocity_report.hpp>
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+#include <cmath>    // abs() for float, and fabs()
 
 namespace sample_module
 {
   using AwCmdType = autoware_auto_control_msgs::msg::AckermannControlCommand;
   // using WhillCmdType = sensor_msgs::msg::Joy;
   using Twist = geometry_msgs::msg::Twist;
+  using Odometry = nav_msgs::msg::Odometry;
+  using AwSteering = autoware_auto_vehicle_msgs::msg::SteeringReport;
+  using AwVelocity = autoware_auto_vehicle_msgs::msg::VelocityReport;
 
 class AwWhillInterface : public rclcpp::Node
 {
@@ -17,12 +25,16 @@ public:
 
 private:
   void callbackAwCmd(const AwCmdType::ConstSharedPtr & msg);
+  void callbackWhillOdom(const Odometry::ConstSharedPtr & msg);
 
 
   rclcpp::Subscription<AwCmdType>::SharedPtr sub_autoware_cmd_;
+  rclcpp::Subscription<Odometry>::SharedPtr sub_whill_odom_;
 
 
   rclcpp::Publisher<Twist>::SharedPtr pub_whill_cmd_;
+  rclcpp::Publisher<AwVelocity>::SharedPtr pub_velocity_status_;
+  rclcpp::Publisher<AwSteering>::SharedPtr pub_steering_status_;
 
 };
 
--- aw_whill_interface/src/aw_whill_interface_node.cpp
+++ aw_whill_interface/src/aw_whill_interface_node.cpp
@@ -8,13 +8,33 @@ AwWhillInterface::AwWhillInterface(const rclcpp::NodeOptions & node_options)
 {
   RCLCPP_INFO(this->get_logger(), "AW WHILL interface launched.");
 
+  // set Subscriber
   sub_autoware_cmd_ = this->create_subscription<AwCmdType>(
     "~/input/autoware_command",
     rclcpp::QoS(1),
     std::bind(&AwWhillInterface::callbackAwCmd, this, std::placeholders::_1)
   );
 
+  sub_whill_odom_ = this->create_subscription<Odometry>(
+    "~/input/whill_odom",
+    rclcpp::QoS(1),
+    std::bind(&AwWhillInterface::callbackWhillOdom, this, std::placeholders::_1)
+  );
+
+  // set Publisher
   pub_whill_cmd_ = this->create_publisher<Twist>("~/output/whill_command", rclcpp::QoS(1));
+
+  pub_velocity_status_ = this->create_publisher<AwVelocity>("~/output/aw_velocity", 1);
+  AwVelocity set_vel;
+  set_vel.longitudinal_velocity = 0.0;
+  set_vel.lateral_velocity = 0.0;
+  set_vel.heading_rate = 0.0;
+  pub_velocity_status_->publish(set_vel);
+
+  pub_steering_status_ = this->create_publisher<AwSteering>("~/output/aw_steering", 1);
+  AwSteering set_steer;
+  set_steer.steering_tire_angle = 0.0;
+  pub_steering_status_->publish(set_steer);
 
 }
 
@@ -27,6 +47,33 @@ void AwWhillInterface::callbackAwCmd(const AwCmdType::ConstSharedPtr & msg)
   output_cmd.angular.z = msg->lateral.steering_tire_angle;
   pub_whill_cmd_->publish(output_cmd);
 
+  return;
+}
+
+void AwWhillInterface::callbackWhillOdom(const Odometry::ConstSharedPtr& msg)
+{
+  AwSteering steer_angle;
+  AwVelocity velocity;
+  double mps_twist = msg->twist.twist.linear.x / 3.6;
+  // double rps_twist = msg->twist.twist.angular.z / 3.6;
+  RCLCPP_INFO(this->get_logger(),"pose  x:%f, y:%f", msg->pose.pose.position.x, msg->pose.pose.position.y);
+  RCLCPP_INFO(this->get_logger(),"twist x:%f, z:%f", msg->twist.twist.linear.x, msg->twist.twist.angular.z);
+  if(mps_twist == 0)
+  {
+    velocity.longitudinal_velocity = 0;
+    steer_angle.steering_tire_angle = 0;
+  }else{
+    // velocity.longitudinal_velocity = mps_twist;
+    // // 速度・角速度から4輪のときの速度・ステア角を求める
+    // steer_angle.steering_tire_angle = std::atan(rps_twist * 0.6 / mps_twist);
+    velocity.longitudinal_velocity = 0;
+    steer_angle.steering_tire_angle = 0;
+  }
+  steer_angle.stamp = msg->header.stamp;
+  velocity.header = msg->header;
+  RCLCPP_INFO(this->get_logger(),"velocity:%f, angle:%f", velocity.longitudinal_velocity, steer_angle.steering_tire_angle);
+  pub_steering_status_->publish(steer_angle);
+  pub_velocity_status_->publish(velocity);
   return;
 }
 
